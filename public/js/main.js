860967063102a6cba063d885df399011c6ec3a7f
TJ Fogarty <fogarty.tj@gmail.com>
2017-01-05 15:49:28 +0000

    Adds ITCSS architecture

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,29 @@
-webpackJsonp([1,2],[,function(n,o,c){(function(n){n(document).ready(function(){console.log("Ready!")})}).call(o,c(0))},function(n,o){},,,function(n,o,c){c(1),n.exports=c(2)}],[5]);
-//# sourceMappingURL=main.js.map
+webpackJsonp([1,2],[
+/* 0 */,
+/* 1 */
+/***/ function(module, exports, __webpack_require__) {
+
+/* WEBPACK VAR INJECTION */(function($) {$(document).ready(function () {
+  $(document).foundation();
+});
+/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(0)))
+
+/***/ },
+/* 2 */
+/***/ function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ },
+/* 3 */,
+/* 4 */,
+/* 5 */
+/***/ function(module, exports, __webpack_require__) {
+
+__webpack_require__(1);
+module.exports = __webpack_require__(2);
+
+
+/***/ }
+],[5]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9zcmMvanMvbWFpbi5qcyIsIndlYnBhY2s6Ly8vLi9zcmMvY3NzL21haW4uc2NzcyJdLCJuYW1lcyI6WyIkIiwiZG9jdW1lbnQiLCJyZWFkeSIsImZvdW5kYXRpb24iXSwibWFwcGluZ3MiOiI7Ozs7O0FBQUEseUNBQUFBLEVBQUVDLFFBQUYsRUFBWUMsS0FBWixDQUFrQixZQUFNO0FBQ3RCRixJQUFFQyxRQUFGLEVBQVlFLFVBQVo7QUFDRCxDQUZELEU7Ozs7Ozs7QUNBQSx5QyIsImZpbGUiOiJwdWJsaWMvanMvbWFpbi5qcyIsInNvdXJjZXNDb250ZW50IjpbIiQoZG9jdW1lbnQpLnJlYWR5KCgpID0+IHtcbiAgJChkb2N1bWVudCkuZm91bmRhdGlvbigpO1xufSk7XG5cblxuXG4vLyBXRUJQQUNLIEZPT1RFUiAvL1xuLy8gLi9zcmMvanMvbWFpbi5qcyIsIi8vIHJlbW92ZWQgYnkgZXh0cmFjdC10ZXh0LXdlYnBhY2stcGx1Z2luXG5cblxuLy8vLy8vLy8vLy8vLy8vLy8vXG4vLyBXRUJQQUNLIEZPT1RFUlxuLy8gLi9zcmMvY3NzL21haW4uc2Nzc1xuLy8gbW9kdWxlIGlkID0gMlxuLy8gbW9kdWxlIGNodW5rcyA9IDEiXSwic291cmNlUm9vdCI6IiJ9
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,3 @@
 $(document).ready(() => {
-  console.log('Ready!');
+  $(document).foundation();
 });
